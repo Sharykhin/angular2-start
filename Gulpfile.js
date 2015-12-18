b7fadcdbdd1e02c8d733472918cc4f74d5608131
Siarhei Sharkhin <siarhei.sharykhin@gmail.com>
2015-12-18 09:03:14 +0300

    add libs

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,9 +5,19 @@ var gulp = require('gulp'),
 
 var tsProject = ts.createProject('src/tsconfig.json');
 
+var libs = [
+	'./node_modules/systemjs/dist/system.src.js',
+	'./node_modules/angular2/bundles/angular2.dev.js'
+];
+
 gulp.task('copy-html', function() {
 	gulp.src('src/**/*.html')
 		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('libs', function() {
+	gulp.src(libs)
+		.pipe(gulp.dest('./dist/vendor'))
 });
 
 gulp.task('build', ['copy-html'], function() {
@@ -17,7 +27,7 @@ gulp.task('build', ['copy-html'], function() {
 	return tsResult.js
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('dist/'));
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
@@ -25,4 +35,4 @@ gulp.task('watch', function() {
 	gulp.watch('src/**/*.html', ['copy-html']);
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['libs', 'build', 'watch']);
